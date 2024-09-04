ae4d59c4619259c1608bde609156669029f04a37
petrambroz <petrambroz@icloud.com>
2024-09-04 17:34:18 +0200

    add init project files

---
+++ AVL/Node.cs
@@ -0,0 +1,16 @@
+﻿namespace AVL;
+
+
+public class Node<T>
+{
+    public T Value;
+    public Node<T>? Left;
+    public Node<T>? Right;
+    public int Height;
+
+    public Node(T value)
+    {
+        Value = value;
+        Height = 1;
+    }
+}
