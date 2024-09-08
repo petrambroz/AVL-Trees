fbf5542822ef99346788411b341d5b0a4f4a6a2b
Petr Ambrož <gentapa59@gmail.com>
2024-09-08 12:05:54 +0200

    add rotate functions, keep balance when inserting a node

--- AVL/AVLTree.cs
+++ AVL/AVLTree.cs
@@ -2,7 +2,9 @@ namespace AVL;
 
 public class AVLTree<T>() where T : System.IComparable
 {
-    public Node<T>? Root { get; private set;  }
+    public Node<T>? Root { get; private set; }
+    public int Count { get; private set; } = 0;
+
 
     public T RootValue()
     {
@@ -20,7 +22,10 @@ public class AVLTree<T>() where T : System.IComparable
     // recursively insert nodes while mainaining balance
     {
         if (node is null)
+        {
+            Count++;
             return new Node<T>(value);
+        }
         if (value.CompareTo(node.Value) < 0)
             node.Left = Insert(node.Left, value);
         else if (value.CompareTo(node.Value) > 0)
@@ -28,10 +33,57 @@ public class AVLTree<T>() where T : System.IComparable
         else return node;
 
         node.Height = 1 + System.Math.Max(Height(node.Left), Height(node.Right));
+        return BalanceNode(node, value);
+    }
+
+    private Node<T> BalanceNode(Node<T> node, T value)
+    {
         int balance = GetBalance(node);
+        if (balance > 1 && value.CompareTo(node.Left.Value) < 0)
+            return RotateRight(node);
 
+        if (balance < -1 && value.CompareTo(node.Right.Value) > 0)
+            return RotateLeft(node);
 
+        if (balance > 1 && value.CompareTo(node.Left.Value) > 0)
+        {
+            node.Left = RotateLeft(node.Left);
+            return RotateRight(node);
+        }
+
+        if (balance < -1 && value.CompareTo(node.Right.Value) < 0)
+        {
+            node.Right = RotateRight(node.Right);
+            return RotateLeft(node);
+        }
         return node;
+    }
+    private Node<T> RotateLeft(Node<T> nodeX)
+    {
+        Node<T> nodeY = nodeX.Right!;
+        Node<T>? alpha = nodeY.Left;
+
+        nodeY.Left = nodeX;
+        nodeX.Right = alpha;
+
+        nodeX.Height = System.Math.Max(Height(nodeX.Left), Height(nodeX.Right)) + 1;
+        nodeY.Height = System.Math.Max(Height(nodeY.Left), Height(nodeY.Right)) + 1;
+
+        return nodeY;
+    }
+
+    private Node<T> RotateRight(Node<T> nodeY)
+    {
+        Node<T> nodeX = nodeY.Left!;
+        Node<T>? alpha = nodeX.Right;
+
+        nodeX.Right = nodeY;
+        nodeY.Left = alpha;
+
+        nodeY.Height = System.Math.Max(Height(nodeY.Left), Height(nodeY.Right)) + 1;
+        nodeX.Height = System.Math.Max(Height(nodeX.Left), Height(nodeX.Right)) + 1;
+
+        return nodeX;
     }
 
     public int GetBalance(Node<T> node)
