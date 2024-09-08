f15fee6c43a742e6c1431bdf6a3ac1886fd0da38
Petr Ambrož <gentapa59@gmail.com>
2024-09-08 12:11:31 +0200

    add some simple unit-tests

---
+++ Unit-Test/UnitTest1.cs
@@ -0,0 +1,63 @@
+namespace Unit_Test;
+
+using AVL;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TestEmptyInsertion()
+    {
+        const int testVal = 5;
+        AVLTree<int> tree = new AVLTree<int>();
+        tree.Insert(testVal);
+        Assert.That(tree.RootValue(), Is.EqualTo(testVal));
+    }
+    [Test]
+    public void TestEmptyTree()
+    //  test if an exception is thrown when trying to access a root value of an empty tree
+    {
+        AVLTree<int> tree = new AVLTree<int>();
+        Assert.Throws<System.Exception>(MethodThatThrows);
+
+        void MethodThatThrows()
+        {
+            tree.RootValue();
+        }
+    }
+    [Test]
+    public void TestSimpleInsert()
+    {
+        AVLTree<int> tree = new AVLTree<int>();
+        tree.Insert(6);
+        tree.Insert(4);
+        tree.Insert(9);
+        Assert.That(tree.Root.Left.Value, Is.EqualTo(4));
+        Assert.That(tree.Root.Right.Value, Is.EqualTo(9));
+
+    }
+
+    [Test]
+    public void TestBalance()
+    {
+        AVLTree<int> tree = new AVLTree<int>();
+        tree.Insert(6);
+        tree.Insert(4);
+        tree.Insert(9);
+        Assert.That(tree.GetBalance(tree.Root), Is.EqualTo(0));
+    }
+    [Test]
+    public void TestBalance2()
+    {
+        AVLTree<int> tree = new AVLTree<int>();
+        tree.Insert(5);
+        tree.Insert(4);
+        tree.Insert(3);
+        tree.Insert(-2);
+        Assert.That(tree.GetBalance(tree.Root), Is.EqualTo(1));
+    }
+}
