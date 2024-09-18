514ceab7f389f5823dfc86c849d4aa79b4e95f5b
Petr Ambrož <gentapa59@gmail.com>
2024-09-18 13:35:52 +0200

    Node class: add Height property, add constructor with non-null children

--- AVL/Node.cs
+++ AVL/Node.cs
@@ -1,16 +1,24 @@
-﻿namespace AVL;
+namespace AVL;
 
 
 public class Node<T>
 {
-    public T Value;
+    public T Value { get; private set; }
     public Node<T>? Left;
     public Node<T>? Right;
-    public int Height;
+    internal int Height;
 
     public Node(T value)
     {
         Value = value;
         Height = 1;
+    }
+
+    public Node(T value, Node<T>? left, Node<T>? right)
+    {
+        Value = value;
+        Height = 1;
+        Left = left;
+        Right = right;
     }
 }
